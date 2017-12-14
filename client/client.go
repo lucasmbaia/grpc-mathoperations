2ce3cfba738e831b086e2dba7441224eba74281d
lucas.martins <lucas.martins@totvs.com.br>
2017-12-14 11:33:54 -0200

    changed version of grpc-base

--- client/client.go
+++ client/client.go
@@ -4,14 +4,12 @@ import (
   "golang.org/x/net/context"
   "google.golang.org/grpc"
   "github.com/lucasmbaia/grpc-base/config"
+  "github.com/lucasmbaia/grpc-base/base"
   "github.com/lucasmbaia/grpc-mathoperations/proto"
-  "google.golang.org/grpc/credentials"
-  "github.com/lucasmbaia/grpc-base/zipkin"
-  "github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
 )
 
 type Config struct {
-  Collector zipkin.Collector
+  base.Config
 }
 
 func init() {
@@ -27,7 +25,7 @@ func (c Config) CalcDouble(ctx context.Context, n *mathoperations.Number) (matho
     value mathoperations.Result
   )
 
-  if conn, err = c.connect(); err != nil {
+  if conn, err = c.ClientConnect(); err != nil {
     return value, err
   }
   defer conn.Close()
@@ -39,30 +37,4 @@ func (c Config) CalcDouble(ctx context.Context, n *mathoperations.Number) (matho
   }
 
   return mathoperations.Result{Value: r.Value}, nil
-}
-
-func (c Config) connect() (*grpc.ClientConn, error) {
-  var (
-    opts  []grpc.DialOption
-    creds credentials.TransportCredentials
-    err   error
-  )
-
-  if config.EnvConfig.GrpcSSL {
-    if creds, err = credentials.NewClientTLSFromFile(config.EnvConfig.CAFile, config.EnvConfig.ServerNameAuthority); err != nil {
-      return new(grpc.ClientConn), err
-    }
-
-    opts = []grpc.DialOption{
-      grpc.WithTransportCredentials(creds),
-      grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(c.Collector.Tracer)),
-    }
-  } else {
-    opts = []grpc.DialOption{
-      grpc.WithInsecure(),
-      grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(c.Collector.Tracer)),
-    }
-  }
-
-  return grpc.Dial(config.EnvLocal.LinkerdURL, opts...)
 }
--- test/client/client_test.go
+++ test/client/client_test.go
@@ -5,6 +5,9 @@ import (
   "testing"
   "github.com/lucasmbaia/grpc-mathoperations/client"
   "github.com/lucasmbaia/grpc-mathoperations/proto"
+  "golang.org/x/net/context"
+  "github.com/lucasmbaia/grpc-base/zipkin"
+  "github.com/lucasmbaia/grpc-base/base"
 )
 
 func TestClientCalcDouble(t *testing.T) {
@@ -13,13 +16,34 @@ func TestClientCalcDouble(t *testing.T) {
     value   mathoperations.Result
     conf    client.Config
     number  mathoperations.Number
+    collector zipkin.Collector
   )
+
+  if collector, err = newCollector() ; err != nil {
+    log.Fatalf("Error to calc fibonacci: ", err)
+  }
 
   number = mathoperations.Number{Value: 10}
 
-  if value, err = conf.CalcDouble(&number); err != nil {
+  conf = client.Config {
+    base.Config {
+      Collector:	collector,
+    },
+  }
+
+  if value, err = conf.CalcDouble(context.Background(), &number); err != nil {
     log.Fatalf("Error to calc double number: ", err)
   }
 
   log.Println(value.Value)
+}
+
+func newCollector() (zipkin.Collector, error) {
+  return zipkin.NewCollector(
+    "http://172.16.95.113:9411/api/v1/spans",
+    "192.168.75.128:9090",
+    "fibonacci",
+    true,
+    false,
+  )
 }
